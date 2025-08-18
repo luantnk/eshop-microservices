44347824923a338199f2826fae3ffe01dfe13fed
luan.tran <nkluantran2907@gmail.com>
2025-08-18 12:49:56 +0700

    feat: push project

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,18 @@
+namespace Catalog.API;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        
+        // Add services to the container
+        
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+      
+
+        app.Run();
+    }
+}
